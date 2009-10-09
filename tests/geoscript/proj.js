e36dc8ed06a89995bd3f8f09e9f0726fe2260984
tschaub <tim.schaub@gmail.com>
2009-10-08 22:18:50 -0600

    Adding Projection constructor and tests.

--- tests/geoscript/proj.js
+++ tests/geoscript/proj.js
@@ -11,6 +11,26 @@ exports.test_transform = function() {
 
 };
 
+exports.test_Projection = function() {
+    var p = new proj.Projection("EPSG:4326");
+    assert.isEqual("EPSG:WGS 84", p.name, "[srid] correct name");
+    assert.isEqual("EPSG:4326", p.code, "[srid] correct code");
+    
+    var wkt = 
+        'GEOGCS[' +
+            '"GCS_WGS_1984",' +
+            'DATUM[' +
+                '"D_WGS_1984",' +
+                'SPHEROID["WGS_1984",6378137,298.257223563]' +
+            '],' +
+            'PRIMEM["Greenwich",0],' +
+            'UNIT["Degree",0.017453292519943295]' +
+        ']';
+    var p2 = new proj.Projection(wkt);
+    assert.isEqual("GCS_WGS_1984", p2.name, "[wkt] correct name");
+    assert.isEqual("EPSG:4326", p2.code, "[wkt] correct code");
+};
+
 if (require.main === module.id) {
     require("test/runner").run(exports);
 }
