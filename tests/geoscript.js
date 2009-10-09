ec08e6e497fb0f63370dd8d922e31990eb4a15c9
tschaub <tim.schaub@gmail.com>
2009-10-08 21:46:53 -0600

    Include feature tests.

--- tests/geoscript.js
+++ tests/geoscript.js
@@ -1,5 +1,6 @@
 exports.test_geom = require("./geoscript/geom");
 exports.test_proj = require("./geoscript/proj");
+exports.test_feature = require("./geoscript/feature");
 
 if (require.main === module.id) {
     require("test/runner").run(exports);
