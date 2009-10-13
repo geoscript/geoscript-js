ab7507c93bf3a3f1f16eeca106122342273d54c2
tschaub <tim.schaub@gmail.com>
2009-10-12 19:08:30 -0600

    Adding util tests to main runner.

--- tests/test_geoscript.js
+++ tests/test_geoscript.js
@@ -1,6 +1,7 @@
 exports["test: geom"] = require("./geoscript/test_geom");
 exports["test: proj"] = require("./geoscript/test_proj");
 exports["test: feature"] = require("./geoscript/test_feature");
+exports["test: util"] = require("./geoscript/util");
 
 if (require.main === module.id) {
     require("test/runner").run(exports);
