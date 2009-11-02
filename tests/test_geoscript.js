206ccda63ce6635a64e0b1bd6d1c4b1b7654dbf5
tschaub <tim.schaub@gmail.com>
2009-11-02 15:30:35 +1100

    Test name typo.

--- tests/test_geoscript.js
+++ tests/test_geoscript.js
@@ -1,7 +1,7 @@
 exports["test: geom"] = require("./geoscript/test_geom");
 exports["test: proj"] = require("./geoscript/test_proj");
 exports["test: feature"] = require("./geoscript/test_feature");
-exports["test: util"] = require("./geoscript/util");
+exports["test: util"] = require("./geoscript/test_util");
 
 if (require.main === module.id) {
     require("test/runner").run(exports);
