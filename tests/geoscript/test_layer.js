712f81bfa0382894db58c10fe9095401844e8257
tschaub <tim.schaub@gmail.com>
2009-11-04 17:22:54 +1100

    Passing test adding feature to memory layer.

--- tests/geoscript/test_layer.js
+++ tests/geoscript/test_layer.js
@@ -1,15 +1,30 @@
 var assert = require("test/assert");
-var Layer = require("geoscript/layer").Layer;
-var MemoryLayer = require("geoscript/layer").MemoryLayer;
+var layer = require("geoscript/layer");
+var geom = require("geoscript/geom");
 
 exports["test: MemoryLayer"] = function() {
 
-    var mem = new MemoryLayer();
+    var mem = new layer.MemoryLayer();
     
-    assert.isTrue(mem instanceof Layer, "instanceof Layer");
-    assert.isTrue(mem instanceof MemoryLayer, "instanceof MemoryLayer");
+    assert.isTrue(mem instanceof layer.Layer, "instanceof Layer");
+    assert.isTrue(mem instanceof layer.MemoryLayer, "instanceof MemoryLayer");    
 
 };
+
+exports["test: MemoryLayer.add"] = function() {
+    
+    var p = new geom.Point([1, 2]);
+    var mem = new layer.MemoryLayer();
+    mem.add({geom: p});
+    
+    assert.isEqual(1, mem.count(), "one item added");
+    
+    var f = mem.features()[0];
+    
+    assert.isSame(p.coordinates, f.geom().coordinates, "first feature has correct geom");
+    
+};
+
 
 if (require.main === module.id) {
     require("test/runner").run(exports);
