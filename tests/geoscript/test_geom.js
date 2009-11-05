9a6d4bbdf06f23ffff0affeaf3ff19465be81011
tschaub <tim.schaub@gmail.com>
2009-11-06 10:50:56 +1100

    Remove unnecessary imports.

--- tests/geoscript/test_geom.js
+++ tests/geoscript/test_geom.js
@@ -1,9 +1,6 @@
-var assert = require("test/assert"),
-    geom = require("geoscript/geom");
-
-exports["test: point"] = require("./geom/test_point");
-exports["test: linestring"] = require("./geom/test_linestring");
-exports["test: polygon"] = require("./geom/test_polygon");
+exports["test: Point"] = require("./geom/test_point");
+exports["test: Linestring"] = require("./geom/test_linestring");
+exports["test: Polygon"] = require("./geom/test_polygon");
 
 if (require.main === module.id) {
     require("test/runner").run(exports);
