90aa400d94526fdd47409966db337816f0eca871
tschaub <tim.schaub@gmail.com>
2009-12-21 16:28:22 -0700

    GeoJSON for features.

--- tests/geoscript/feature/test_feature.js
+++ tests/geoscript/feature/test_feature.js
@@ -2,7 +2,7 @@ var assert = require("test/assert"),
     geom = require("geoscript/geom"),
     feature = require("geoscript/feature");
 
-exports["test: Feature.constructor"] = function() {
+exports["test: constructor"] = function() {
     
     var values = {
         name: "Some Location",
@@ -18,6 +18,41 @@ exports["test: Feature.constructor"] = function() {
     assert.is(values.location, f.get("location"), "correct location value using get");
     assert.is(values.location, f.geometry, "correct location value using geometry");
     
+};
+
+exports["test: json"] = function() {
+
+    var values = {
+        name: "Some Location",
+        location: new geom.Point([1, 2]),
+        population: 100
+    };
+    
+    var f = new feature.Feature({values: values});
+    
+    var json = f.json;
+    var obj, msg;
+    try {
+        obj = JSON.decode(json);
+    } catch(err) {
+        msg = err.message;
+    }
+    if (obj) {
+        assert.is("Feature", obj.type, "correct type");
+        var props = {
+            name: values.name,
+            population: values.population
+        };
+        assert.isSame(props, obj.properties, "correct properties");
+        var g = obj.geometry;
+        assert.is("Point", g.type, "correct geometry type");
+        assert.isSame([1, 2], g.coordinates, "correct geometry coordinates");
+    } else {
+        assert.isTrue(false, "invalid json: " + msg);
+    }
+    
+    
+
 };
 
 if (require.main === module.id) {
