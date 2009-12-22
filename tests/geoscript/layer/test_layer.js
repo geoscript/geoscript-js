3508e0275afe0fbc129efbb198aa4e00f02b64f3
tschaub <tim.schaub@gmail.com>
2009-12-22 12:03:07 -0700

    Add clone method for layers.

--- tests/geoscript/layer/test_layer.js
+++ tests/geoscript/layer/test_layer.js
@@ -25,3 +25,33 @@ exports["test: temporary"] = function() {
 
     
 };
+
+exports["test: clone"] = function() {
+
+    var clone;
+    var temp = new Layer({name: "foo"});
+
+    // create a clone without providing a name
+    clone = temp.clone();
+    assert.isTrue(clone instanceof Layer, "clone is a Layer");
+    assert.isTrue(typeof clone.name === "string", "clone has a name");
+    assert.isTrue(clone.name !== temp.name, "clone gets a new name");
+    
+    // create a clone with a new name
+    clone = temp.clone("bar");
+    assert.is("bar", clone.name, "clone can be given a new name");
+    
+    // clone an existing layer with features
+    var file = require("file");
+    var path = file.resolve(module.path, "../../data");
+    
+    var shp = new Layer({
+        workspace: path,
+        name: "states"
+    });
+    
+    clone = shp.clone();
+    assert.isTrue(clone.temporary, "clone is a temporary layer");
+    assert.is(shp.count, clone.count, "clone has same count as original");
+
+};
