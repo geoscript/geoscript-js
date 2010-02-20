5dd68a2cebcb1a3a65add70d02655207fd630615
tschaub <tim.schaub@gmail.com>
2010-02-20 14:30:00 -0500

    Proper filter serialization to xml.

--- tests/geoscript/test_filter.js
+++ tests/geoscript/test_filter.js
@@ -63,6 +63,23 @@ exports["test Filter.evaluate"] = function() {
 
 };
 
+exports["test Filter.toXML"] = function() {
+    
+    var cql = "name = 'foo'";
+    var f = new filter.Filter(cql);
+    var xml = '<ogc:Filter xmlns:ogc="http://www.opengis.net/ogc" xmlns:gml="http://www.opengis.net/gml"><ogc:PropertyIsEqualTo><ogc:PropertyName>name</ogc:PropertyName><ogc:Literal>foo</ogc:Literal></ogc:PropertyIsEqualTo></ogc:Filter>';
+    assert.is(xml, f.toXML(), "correct xml");
+    
+};
+
+exports["test Filter.fromXML"] = function() {
+    
+    var xml = '<ogc:Filter xmlns:ogc="http://www.opengis.net/ogc" xmlns:gml="http://www.opengis.net/gml"><ogc:PropertyIsEqualTo><ogc:PropertyName>name</ogc:PropertyName><ogc:Literal>foo</ogc:Literal></ogc:PropertyIsEqualTo></ogc:Filter>';
+    var f = new filter.Filter.fromXML(xml);
+    var cql = "name = 'foo'";
+    assert.is(cql, f.cql, "fromXML produces good filter");
+    
+};
 
 if (require.main === module.id) {
     require("test/runner").run(exports);
