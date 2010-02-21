447674036d2a49cc5042beaa75622cadc5dd2766
tschaub <tim.schaub@gmail.com>
2010-02-21 14:10:28 -0500

    Use common tests for shapefiles.

---
+++ tests/geoscript/layer/test_shapefile.js
@@ -0,0 +1,22 @@
+var Layer = require("geoscript/layer").Layer;
+var COMMON = require("./common");
+var ADMIN = require("../../admin");
+
+exports.setup = ADMIN.shp.setup;
+exports.teardown = ADMIN.shp.teardown;
+
+var getLayer = function() {
+    return new Layer({
+        workspace: ADMIN.shp.dest,
+        name: "states"
+    });
+};
+
+// pull in all common tests
+for (var test in COMMON) {
+    exports[test] = COMMON[test](getLayer);
+}
+
+if (require.main === module.id) {
+    require("test/runner").run(exports);
+}
