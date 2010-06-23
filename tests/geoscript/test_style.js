1ee12d2e68a7ac1ca92a7e466cd19fa9beaaca70
tschaub <tim.schaub@gmail.com>
2010-06-23 14:31:58 -0400

    Adding line symbolizer.

--- tests/geoscript/test_style.js
+++ tests/geoscript/test_style.js
@@ -1,6 +1,8 @@
 
 exports["test: PointSymbolizer"] = require("./style/test_point");
 
+exports["test: LineSymbolizer"] = require("./style/test_line");
+
 if (require.main == module.id) {
     require("test").run(exports);
 }
