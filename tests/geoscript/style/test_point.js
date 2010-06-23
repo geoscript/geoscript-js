1230a334533c6aa3229d03664dec376772f30a2b
tschaub <tim.schaub@gmail.com>
2010-06-23 15:06:51 -0400

    Point symbolizer doc and test updates.

--- tests/geoscript/style/test_point.js
+++ tests/geoscript/style/test_point.js
@@ -4,79 +4,99 @@ var STYLE = require("geoscript/style");
 
 exports["test: constructor"] = function() {
     
-    var point = new STYLE.PointSymbolizer({});
+    var symbolizer = new STYLE.PointSymbolizer({});
     
-    ASSERT.ok(point instanceof STYLE.Symbolizer, "PointSymbolizer is a Symbolizer");
-    ASSERT.ok(point instanceof STYLE.PointSymbolizer, "PointSymbolizer is a PointSymbolizer");
+    ASSERT.ok(symbolizer instanceof STYLE.Symbolizer, "PointSymbolizer is a Symbolizer");
+    ASSERT.ok(symbolizer instanceof STYLE.PointSymbolizer, "PointSymbolizer is a PointSymbolizer");
 
-    var point = new STYLE.PointSymbolizer({
+    var symbolizer = new STYLE.PointSymbolizer({
         shape: "circle",
         strokeColor: "#ff00ff",
         fillOpacity: 0.7
     });
-    ASSERT.strictEqual(point.shape, "circle", "shape properly set");
-    ASSERT.strictEqual(point.strokeColor, "#ff00ff", "stroke color properly set");
-    ASSERT.strictEqual(point.fillOpacity, 0.7, "fill opacity properly set");
+    ASSERT.strictEqual(symbolizer.shape, "circle", "shape properly set");
+    ASSERT.strictEqual(symbolizer.strokeColor, "#ff00ff", "stroke color properly set");
+    ASSERT.strictEqual(symbolizer.fillOpacity, 0.7, "fill opacity properly set");
     
 };
 
 exports["test: shape"] = function() {
     
-    var point = new STYLE.PointSymbolizer({});
-    ASSERT.strictEqual(point.shape, "square", "square mark by default");
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.strictEqual(symbolizer.shape, "square", "square mark by default");
     
-    point.shape = "star";
-    ASSERT.strictEqual(point.shape, "star", "shape properly set");
+    symbolizer.shape = "star";
+    ASSERT.strictEqual(symbolizer.shape, "star", "shape properly set");
+    
+};
+
+exports["test: size"] = function() {
+    
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.ok(isNaN(symbolizer.size), "size NaN by default");
+    
+    symbolizer.size = 10;
+    ASSERT.strictEqual(symbolizer.size, 10, "size properly set");
+    
+};
+
+exports["test: rotation"] = function() {
+    
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.strictEqual(symbolizer.rotation, 0, "rotation 0 by default");
+    
+    symbolizer.rotation = 45;
+    ASSERT.strictEqual(symbolizer.rotation, 45, "rotation properly set");
     
 };
 
 exports["test: strokeColor"] = function() {
     
-    var point = new STYLE.PointSymbolizer({});
-    ASSERT.strictEqual(point.strokeColor, "#000000", "stroke color black by default");
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.strictEqual(symbolizer.strokeColor, "#000000", "stroke color black by default");
 
-    point.strokeColor = "#ff0000";
-    ASSERT.strictEqual(point.strokeColor, "#ff0000", "stroke color properly set");
+    symbolizer.strokeColor = "#ff0000";
+    ASSERT.strictEqual(symbolizer.strokeColor, "#ff0000", "stroke color properly set");
     
 };
 
-exports["test: strokeColor"] = function() {
+exports["test: strokeWdith"] = function() {
 
-    var point = new STYLE.PointSymbolizer({});
-    ASSERT.strictEqual(point.strokeWidth, 1, "stroke width 1 by default");
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.strictEqual(symbolizer.strokeWidth, 1, "stroke width 1 by default");
 
-    point.strokeWidth = 3;
-    ASSERT.strictEqual(point.strokeWidth, 3, "stroke width properly set");
+    symbolizer.strokeWidth = 3;
+    ASSERT.strictEqual(symbolizer.strokeWidth, 3, "stroke width properly set");
 
 };
 
-exports["test: strokeColor"] = function() {
+exports["test: strokeOpacity"] = function() {
 
-    var point = new STYLE.PointSymbolizer({});
-    ASSERT.strictEqual(point.strokeOpacity, 1, "stroke opacity 1 by default");
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.strictEqual(symbolizer.strokeOpacity, 1, "stroke opacity 1 by default");
 
-    point.strokeOpacity = 0.8;    
-    ASSERT.strictEqual(point.strokeOpacity, 0.8, "stroke opacity properly set");
+    symbolizer.strokeOpacity = 0.8;    
+    ASSERT.strictEqual(symbolizer.strokeOpacity, 0.8, "stroke opacity properly set");
     
 };
 
 exports["test: fillColor"] = function() {
     
-    var point = new STYLE.PointSymbolizer({});
-    ASSERT.strictEqual(point.fillColor, "#808080", "fill color gray by default");
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.strictEqual(symbolizer.fillColor, "#808080", "fill color gray by default");
 
-    point.fillColor = "#00FF00";
-    ASSERT.strictEqual(point.fillColor, "#00FF00", "fill color properly set");
+    symbolizer.fillColor = "#00FF00";
+    ASSERT.strictEqual(symbolizer.fillColor, "#00FF00", "fill color properly set");
 
 };
 
 exports["test: fillOpacity"] = function() {
 
-    var point = new STYLE.PointSymbolizer({});
-    ASSERT.strictEqual(point.fillOpacity, 1, "fill opcity 1 by default");
+    var symbolizer = new STYLE.PointSymbolizer({});
+    ASSERT.strictEqual(symbolizer.fillOpacity, 1, "fill opcity 1 by default");
 
-    point.fillOpacity = 0.8;    
-    ASSERT.strictEqual(point.fillOpacity, 0.8, "fill opacity properly set");
+    symbolizer.fillOpacity = 0.8;    
+    ASSERT.strictEqual(symbolizer.fillOpacity, 0.8, "fill opacity properly set");
     
 };
 
