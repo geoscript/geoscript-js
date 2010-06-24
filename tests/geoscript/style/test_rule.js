7c4e42a9b3686bcf82441a6067ac2c7d192e2c9a
tschaub <tim.schaub@gmail.com>
2010-06-23 23:08:21 -0400

    Add rule constructor.

---
+++ tests/geoscript/style/test_rule.js
@@ -0,0 +1,28 @@
+var ASSERT = require("assert");
+var STYLE = require("geoscript/style");
+var Filter = require("geoscript/filter").Filter;
+
+exports["test: constructor"] = function() {
+    
+    var rule = new STYLE.Rule({});
+    
+    ASSERT.ok(rule instanceof STYLE.Rule, "rule is a Rule");
+
+    var rule = new STYLE.Rule({
+        minScaleDenominator: 100000,
+        filter: "STATE_ABBR = 'TX'",
+        symbolizers: [{
+            type: "PolygonSymbolizer",
+            fillColor: "#0000ff"
+        }]
+    });
+    ASSERT.strictEqual(rule.minScaleDenominator, 100000, "min scale denominator set");
+    ASSERT.ok(rule.filter instanceof Filter, "filter is a Filter");
+    ASSERT.strictEqual(rule.symbolizers.length, 1, "one symbolizer");
+    ASSERT.ok(rule.symbolizers[0] instanceof STYLE.PolygonSymbolizer, "point symbolizer");
+    
+};
+
+if (require.main == module.id) {
+    require("test").run(exports);
+}
--- tests/geoscript/test_style.js
+++ tests/geoscript/test_style.js
@@ -5,6 +5,8 @@ exports["test: LineSymbolizer"] = require("./style/test_line");
 
 exports["test: PolygonSymbolizer"] = require("./style/test_polygon");
 
+exports["test: Rule"] = require("./style/test_rule");
+
 if (require.main == module.id) {
     require("test").run(exports);
 }
