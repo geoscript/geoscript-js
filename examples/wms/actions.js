93ea2aeb46aec11c47e222cf680df265b5a12c37
tschaub <tim.schaub@gmail.com>
2010-06-27 13:05:21 -0600

    Updated WMS example.

---
+++ examples/wms/actions.js
@@ -0,0 +1,49 @@
+var FS = require("fs");
+var Stream = require("io").Stream;
+var ByteArray = require("binary").ByteArray;
+var Request = require("ringo/webapp/request").Request;
+var skinResponse = require("ringo/webapp/response").skinResponse;
+var Map = require("geoscript/map").Map;
+
+var map = new Map({
+    layers: [{
+        name: "states",
+        workspace: FS.join(module.directory, "..", "data", "shapefiles"),
+        style: {
+            fillColor: "steelblue",
+            strokeColor: "wheat"
+        }
+    }]
+});
+
+exports.index = function(req) {
+
+    return skinResponse("skins/index.html", {
+        host: req.host,
+        port: req.port,
+        scriptName: req.scriptName,
+        pathInfo: req.pathInfo
+    });
+
+};
+
+exports.wms = function(req) {
+
+    var request = new Request(req);
+    var bbox = request.queryParams["BBOX"].split(",").map(Number);
+    var stream = new Stream(new java.io.ByteArrayOutputStream());
+
+    map.render({
+        imageType: "png",
+        bounds: bbox,
+        output: stream
+    });
+
+    var body = new ByteArray(stream.unwrap().toByteArray());
+    return {
+        status: 200,
+        headers: {"Content-Type": "image/png"},
+        body: [body]
+    };
+
+};
--- examples/wms/config.js
+++ examples/wms/config.js
@@ -1,38 +1,5 @@
-var FS = require("fs");
-var Stream = require("io").Stream;
-var ByteArray = require("binary").ByteArray;
-var Request = require("ringo/webapp/request").Request;
-var Map = require("geoscript/map").Map;
+exports.urls = [
+    ["/", "actions"],
+];
 
-var map = new Map({
-    layers: [{
-        name: "states",
-        workspace: FS.join(module.directory, "..", "data", "shapefiles"),
-        style: {
-            fillColor: "#ff9900",
-            strokeColor: "#663300"
-        }
-    }]
-});
-
-exports.app = function(env) {
-
-    var request = new Request(env);
-    var bbox = request.queryParams["BBOX"].split(",").map(Number);
-    var stream = new Stream(new java.io.ByteArrayOutputStream());
-
-    map.render({
-        imageType: "png",
-        bounds: bbox,
-        output: stream
-    });
-
-    var body = new ByteArray(stream.unwrap().toByteArray());
-    return {
-        status: 200,
-        headers: {"Content-Type": "image/png"},
-        body: [body]
-    };
-
-};
-
+exports.app = require("ringo/webapp").handleRequest;
