968880f4244112915b28680dca8dfa46cf36e924
tschaub <tim.schaub@gmail.com>
2010-06-28 09:36:06 -0600

    Simplifying app code by returning having map.render return a ByteArray.  Will bring streams back in later.

--- examples/wms/actions.js
+++ examples/wms/actions.js
@@ -1,6 +1,4 @@
 var FS = require("fs");
-var Stream = require("io").Stream;
-var ByteArray = require("binary").ByteArray;
 var Request = require("ringo/webapp/request").Request;
 var skinResponse = require("ringo/webapp/response").skinResponse;
 var Map = require("geoscript/map").Map;
@@ -31,19 +29,16 @@ exports.wms = function(req) {
 
     var request = new Request(req);
     var bbox = request.queryParams["BBOX"].split(",").map(Number);
-    var stream = new Stream(new java.io.ByteArrayOutputStream());
 
-    map.render({
+    var image = map.render({
         imageType: "png",
-        bounds: bbox,
-        output: stream
+        bounds: bbox
     });
 
-    var body = new ByteArray(stream.unwrap().toByteArray());
     return {
         status: 200,
         headers: {"Content-Type": "image/png"},
-        body: [body]
+        body: [image]
     };
 
 };
--- examples/wms/config.js
+++ examples/wms/config.js
@@ -1,9 +1,13 @@
 exports.urls = [
     ["/", "actions"],
 ];
-
-exports.app = require("ringo/webapp").handleRequest;
-
+exports.middleware = [
+    "ringo/middleware/etag",
+    "ringo/middleware/notfound"
+];
 exports.httpConfig = {
     staticDir: "static"
 };
+
+exports.app = require("ringo/webapp").handleRequest;
+
