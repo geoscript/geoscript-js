9d668007211da99ea16704cea8bff7387d629600
Tim Schaub <tim.schaub@gmail.com>
2010-11-14 17:56:42 -0700

    Stream GeoJSON feature collection with support for start and count parameters.

--- examples/sfs/layers/features/index.js
+++ examples/sfs/layers/features/index.js
@@ -1,10 +1,42 @@
+var layers = require("../../config").layers;
 
-exports.GET = function(req) {
+exports.GET = function(req, parts) {
+    var params = req.queryParams || {};
+    
+    var len = parts.length;
+    var layerId = parts[len-3];
+    
+    var layer = layers[layerId];
+    if (!layer) {
+        throw {notfound: true};
+    }
+    
+    var header = '{"type": "FeatureCollection", "features": [';
+    var footer = ']}\n';
+    
+    var cursor = layer.features;
+    if (params.start) {
+        print("skipping: " + Number(params.start))
+        cursor.skip(Number(params.start));
+    }
+    var count = params.count && Number(params.count);
+    
+    var body = {
+        forEach: function(callback) {
+            callback(header);
+            cursor.forEach(function(feature, index) {
+                callback((index && ", " || "") + feature.json);
+                return count ? (index < count-1) : true;
+            });
+            cursor.close();
+            callback(footer);
+        }
+    }
     
     return {
         status: 200,
         headers: {"Content-Type": "text/plain"},
-        body: ["feature collections not yet implemented"]
+        body: body
     };
     
 };
