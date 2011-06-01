3cceea76edccbb2b7e8807c6ffc27b361cd17f1e
tschaub <tim.schaub@gmail.com>
2011-06-01 17:51:10 -0600

    Using ringo.modulepath instead of manually manipulating require.paths (thanks rrmckinley).

--- tests/all.js
+++ tests/all.js
@@ -1,10 +1,3 @@
-// TODO: determine if this can be avoided - seems necessary to use run.jar 
-// if geoscript is not in ringojs/packages
-var lib = getResource("../lib");
-if (require.paths.indexOf(lib) === -1) {
-    require.paths.push(lib);
-}
-
 exports["test: geoscript"] = require("./test_geoscript");
 
 if (require.main == module || require.main == module.id) {
