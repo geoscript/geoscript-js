163a31d905b856d1d3d3071ed4dc3dcaac9b3672
Tim Schaub <tim.schaub@gmail.com>
2011-09-05 08:50:40 -0600

    Interpolate literals in the same way.

--- examples/style/gradient.js
+++ examples/style/gradient.js
@@ -1,5 +1,5 @@
 var Directory = require("geoscript/workspace").Directory;
-var {Fill, gradient} = require("geoscript/style");
+var {Fill, Shape, gradient} = require("geoscript/style");
 var Map = require("geoscript/map").Map;
 
 var states = Directory("data").get("states");
@@ -7,7 +7,11 @@ var states = Directory("data").get("states");
 states.style = gradient({
     expression: "PERSONS / LAND_KM", 
     values: [0, 200], 
-    styles: [Fill("#000066"), Fill("red")], 
+    // styles: [Fill("#000066"), Fill("red")], 
+    styles: [
+        Shape({name: "circle", size: 2, fill: "#000066"}), 
+        Shape({name: "circle", size: 9, fill: "red"})
+    ], 
     classes: 10, 
     method: "exponential"
 }).and(
