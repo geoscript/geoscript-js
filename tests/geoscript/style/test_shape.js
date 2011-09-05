2685e54ec1fd325a9259c843a54f58b7af2030e1
tschaub <tim.schaub@gmail.com>
2011-09-05 10:53:39 -0600

    Tests for shape fill, stroke, and clone.

--- tests/geoscript/style/test_shape.js
+++ tests/geoscript/style/test_shape.js
@@ -1,6 +1,8 @@
 var ASSERT = require("assert");
 var STYLE = require("geoscript/style");
 var Expression = require("geoscript/filter").Expression;
+var Fill = require("geoscript/style").Fill;
+var Stroke = require("geoscript/style").Stroke;
 
 exports["test: constructor"] = function() {
     
@@ -119,6 +121,78 @@ exports["test: rotation"] = function() {
     ASSERT.strictEqual(shape.rotation.text, "foo", "rotation from expression");
 
 };
+
+exports["test: fill"] = function() {
+    
+    var shape;
+    
+    // fill in config
+    shape = new STYLE.Shape({fill: "blue"});
+    ASSERT.ok(shape.fill instanceof Fill, "fill is Fill");
+    ASSERT.strictEqual(shape.fill.brush.hex, "0000ff", "correct fill color");
+
+    // literal for fill
+    shape = new STYLE.Shape();
+    shape.fill = "red";
+    ASSERT.ok(shape.fill instanceof Fill, "fill is Fill");
+    ASSERT.strictEqual(shape.fill.brush.hex, "ff0000", "correct fill color");
+
+    // config for fill
+    shape = new STYLE.Shape();
+    shape.fill = {brush: "red", opacity: 0.25};
+    ASSERT.ok(shape.fill instanceof Fill, "fill is Fill");
+    ASSERT.strictEqual(shape.fill.brush.hex, "ff0000", "correct fill color");
+    ASSERT.strictEqual(shape.fill.opacity.text, "0.25", "correct fill opacity");
+    
+};
+
+exports["test: stroke"] = function() {
+    
+    var shape;
+    
+    // stroke in config
+    shape = new STYLE.Shape({stroke: "blue"});
+    ASSERT.ok(shape.stroke instanceof Stroke, "stroke is Stroke");
+    ASSERT.strictEqual(shape.stroke.brush.hex, "0000ff", "correct stroke color");
+
+    // literal for stroke
+    shape = new STYLE.Shape();
+    shape.stroke = "red";
+    ASSERT.ok(shape.stroke instanceof Stroke, "stroke is Stroke");
+    ASSERT.strictEqual(shape.stroke.brush.hex, "ff0000", "correct stroke color");
+
+    // config for fill
+    shape = new STYLE.Shape();
+    shape.stroke = {brush: "red", width: 3};
+    ASSERT.ok(shape.stroke instanceof Stroke, "stroke is Stroke");
+    ASSERT.strictEqual(shape.stroke.brush.hex, "ff0000", "correct stroke color");
+    ASSERT.strictEqual(shape.stroke.width.text, "3", "correct stroke width");
+    
+};
+
+exports["test: clone"] = function() {
+    
+    var shape = new STYLE.Shape({
+        name: "cross",
+        size: 9,
+        rotation: new Expression("orientation"),
+        fill: "blue",
+        stroke: {brush: "white", width: 3},
+        opacity: 0.75
+    });
+    
+    var clone = shape.clone();
+    
+    ASSERT.strictEqual(clone.name.text, shape.name.text, "clone name");
+    ASSERT.strictEqual(clone.opacity.text, shape.opacity.text, "clone opacity");
+    ASSERT.strictEqual(clone.size.text, shape.size.text, "clone size");
+    ASSERT.strictEqual(clone.rotation.text, shape.rotation.text, "clone rotation");
+    ASSERT.strictEqual(clone.fill.brush.hex, shape.fill.brush.hex, "clone fill color");
+    ASSERT.strictEqual(clone.stroke.brush.hex, shape.stroke.brush.hex, "clone stroke color");
+    ASSERT.strictEqual(clone.stroke.opacity.text, shape.stroke.opacity.text, "clone stroke opacity");
+    
+}
+
 
 if (require.main == module.id) {
     system.exit(require("test").run(exports));
