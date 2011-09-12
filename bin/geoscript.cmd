7ceef481f1d015c78c85503224f3545c7c4d49f4
Tim Schaub <tim.schaub@gmail.com>
2011-09-12 10:13:41 -0600

    Attempt at win launcher.  Rhino failing.

---
+++ bin/geoscript.cmd
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+:: GEOSCRIPT_HOME is the parent of the bin directory
+set GEOSCRIPT_HOME=%~dp0..
+
+:: put GeoTools jars on the classpath
+setlocal enabledelayedexpansion
+echo. >NUL 2>tmp.txt
+for /R %GEOSCRIPT_HOME%\jars %%x in (*.jar) do echo %%x >> tmp.txt
+
+set CP=
+for /f %%a in (tmp.txt) do (
+    set CP=!CP!;%%a
+)
+del /q tmp.txt
+
+set CLASS=org.mozilla.javascript.tools.shell.Main
+
+java -cp %CP% %CLASS% -version 180 -modules %GEOSCRIPT_HOME%\lib
+
