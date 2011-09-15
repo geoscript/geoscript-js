6d39034db2d7d1819a1d88d6ae16b4a2eee84f96
Tim Schaub <tim.schaub@gmail.com>
2011-09-15 03:05:29 -0600

    Allow options while adding layers to a workspace.

--- tests/geoscript/workspace/test_directory.js
+++ tests/geoscript/workspace/test_directory.js
@@ -54,6 +54,32 @@ exports["test: get"] = function() {
     
 };
 
+exports["test: add"] = function() {
+
+    var dir = new WORKSPACE.Directory(dataDir);
+    var states = dir.get("states");
+    
+    ASSERT.throws(function() {
+        dir.add(states);
+    }, Error, "add with same name");
+    
+    var states2 = dir.add(states, {name: "states2"});
+    ASSERT.equal(dir.names.length, 2, "two layers");
+    ASSERT.equal(states2.count, 49, "two count");
+    
+    var states3 = dir.add(states, {name: "states3", filter: "STATE_ABBR like 'M%'"});
+    ASSERT.equal(dir.names.length, 3, "three layers");
+    ASSERT.equal(states3.count, 8, "three count");
+
+    var states4 = dir.add(states, {name: "states4", projection: "epsg:32002"});
+    ASSERT.equal(dir.names.length, 4, "four layers");
+    ASSERT.equal(states4.count, 49, "four count");
+    ASSERT.equal(states4.projection.id, "EPSG:32002", "four projection");
+
+};
+
+
+
 if (require.main == module.id) {
     system.exit(require("test").run(exports));
 }
