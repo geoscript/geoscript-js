1f5a06ac65353f4b83e07e1ec443964c949a4459
Tim Schaub <tim.schaub@gmail.com>
2012-06-09 18:16:41 -0600

    Wrap multipart geometries.

--- src/main/java/org/geoscript/js/geom/GeometryWrapper.java
+++ src/main/java/org/geoscript/js/geom/GeometryWrapper.java
@@ -14,6 +14,14 @@ public class GeometryWrapper {
                 wrapped = new LineString(scope, (com.vividsolutions.jts.geom.LineString) geometry);
             } else if (geometry instanceof com.vividsolutions.jts.geom.Polygon) {
                 wrapped = new Polygon(scope, (com.vividsolutions.jts.geom.Polygon) geometry);
+            } else if (geometry instanceof com.vividsolutions.jts.geom.GeometryCollection) {
+                wrapped = new GeometryCollection(scope, (com.vividsolutions.jts.geom.GeometryCollection) geometry);
+            } else if (geometry instanceof com.vividsolutions.jts.geom.MultiPoint) {
+                wrapped = new MultiPoint(scope, (com.vividsolutions.jts.geom.MultiPoint) geometry);
+            } else if (geometry instanceof com.vividsolutions.jts.geom.MultiLineString) {
+                wrapped = new MultiLineString(scope, (com.vividsolutions.jts.geom.MultiLineString) geometry);
+            } else if (geometry instanceof com.vividsolutions.jts.geom.MultiPolygon) {
+                wrapped = new MultiPolygon(scope, (com.vividsolutions.jts.geom.MultiPolygon) geometry);
             }
         } catch (Exception e) {
             throw new RuntimeException("Trouble wrapping geometry", e);
