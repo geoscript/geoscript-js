744024392ffe81f6bd577c1f593ee846fe07fe94
tschaub <tim.schaub@gmail.com>
2012-07-20 15:12:58 -0600

    Document geom/io/json module.

--- src/main/resources/org/geoscript/js/lib/geoscript/geom/io/json.js
+++ src/main/resources/org/geoscript/js/lib/geoscript/geom/io/json.js
@@ -1,6 +1,40 @@
+/** api: module = geom/io/json */
+
+/** api: synopsis
+ *  Reading and writing JSON.
+ */
+
+/** api: summary
+ *  The :mod:`geom/io/json` module exports functions to read and write 
+ *  geometries.
+ *
+ *  .. code-block:: javascript
+ *  
+ *      js> var json = require("geoscript/geom/io/json");
+ */
+
 var UTIL = require("../../util");
 var GEOM = require("../../geom");
 
+/** api: method[read]
+ *  :arg str: ``String``
+ *  :returns: :class:`geom.Geometry`
+ *  
+ *  Creates a geometry from a JSON string.  See http://geojson.org/ for details
+ *  on the format.
+ *
+ *  Example use:
+ *
+ *  .. code-block:: javascript
+ *  
+ *      js> var json = require("geoscript/geom/io/json");
+ *      js> var str = '{"type": "Point", "coordinates": [0, 1]}';
+ *      js> var point = json.read(str);
+ *      js> point.x
+ *      0
+ *      js> point.y
+ *      1
+ */
 var read = function(str) {
     
     var obj = JSON.parse(str);
@@ -26,6 +60,24 @@ var read = function(str) {
 
 };
 
+/** api: method[write]
+ *  :arg geometries: :class:`geom.Geometry`
+ *  :returns: ``String``
+ *  
+ *  Serializes a geometry as a JSON string.  See http://geojson.org/ for details
+ *  on the format.
+ *
+ *  Example use:
+ *
+ *  .. code-block:: javascript
+ *  
+ *      js> var json = require("geoscript/geom/io/json");
+ *      js> var Point = require("geoscript/geom").Point;
+ *      js> var point = new Point([0, 1]);
+ *      js> var str = json.write(point);
+ *      js> str
+ *      {"type": "Point", "coordinates": [0, 1]}
+ */
 var write = function(geometries) {
     
     var collection = true;
